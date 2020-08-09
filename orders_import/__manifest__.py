8104e22972ca186f8c0de606c82ce0c41acba77c
Sagar Patel <grant@momentumaero.com>
2020-08-09 17:41:24 +0400

    [ADD] module for manual order import/invoices

---
+++ orders_import/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Bista Solutions Pvt. Ltd
+#    Copyright (C) 2017 (http://www.bistasolutions.com)
+#
+##############################################################################
+{
+    'name': 'Orders Import',
+    'version': '1.1',
+    'category': 'Sales',
+    'description': """
+   Import Bulk Sale Order Using from Excel or CSV File.
+    """,
+    'author': 'Shawaz Jahangiri',
+    'website': 'https://www.bistasolutions.com',
+    'depends': ['base', 'sale'],
+    'data': [
+        'wizard/order_import_wiz_view.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ orders_import/models/sale.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api, _
+
+
+class SaleOrder(models.Model):
+    _inherit = 'sale.order'
+
+    import_order_id = fields.Char('Order ID')
