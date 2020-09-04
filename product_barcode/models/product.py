2f902a20905492a4d1dff7ec360180c4dee321c2
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-09-04 13:55:21 +0530

    [IMP] Improve product barcode label.

--- product_barcode/models/product.py
+++ product_barcode/models/product.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api, _
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    unit_barcode = fields.Char('Product Unit Barcodes')
+    unit_barcode = fields.Char('Unit Barcode')
 
     _sql_constraints = [
         ('unit_barcode_uniq', 'unique(unit_barcode)', 'A Unit barcode can only be assigned to one product !')]
