cd838811636e56396b98471d082a9f15daf91c00
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-09-04 17:34:14 +0530

    [ADD] Added barcode lot serial label.

---
+++ barcode_label/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Lot Barcode Label",
+
+    'summary': """Lot Barcode""",
+
+    'description': """
+                Lot barcode report customization.
+            """,
+
+    'author': "Bista Solutions",
+    'website': "http://www.bistasolutions.com",
+    'category': 'Product',
+    'version': '1.1',
+    'depends': ['base', 'stock', 'product', 'barcodes'],
+    # always loaded
+    'data': [
+        'data/lot_report_paperformat.xml',
+        'report/report_lot_barcode.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [],
+}
