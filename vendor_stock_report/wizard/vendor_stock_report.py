7cde598cb3ec23afbd977ddcf95fd61cd60f901f
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-09-10 20:56:50 +0530

    [IMP] Improved in vendor stock report to show all products.

--- vendor_stock_report/wizard/vendor_stock_report.py
+++ vendor_stock_report/wizard/vendor_stock_report.py
@@ -52,8 +52,7 @@ class WizVendorStockReport(models.TransientModel):
         value_format = workbook.add_format({'align': 'left', 'text_wrap': True})
         only_bold = workbook.add_format({'bold': True, 'align': 'left', 'text_wrap': True})
         #         sheet.freeze_panes(6, 3)
-        sheet.set_column('A:A', 35)
-        sheet.set_column('B:K', 14)
+
         # sheet.set_column('E:E', 18)
         # sheet.set_column('F:F', 40)
         # sheet.set_column('G:N', 18)
@@ -64,6 +63,8 @@ class WizVendorStockReport(models.TransientModel):
         # report_title = 'Vendor Stock Report'
         # sheet.merge_range(rows, cols, rows, cols + 6,
         #                   report_title, title_format)
+
+        existing_product_list = []
         vendor_list = self.env['product.supplierinfo'].search([('product_tmpl_id.qty_available', '!=', 0.0)],
                                                               order='name').mapped('name')
         vendor_list += stock_obj.search([('partner_id', '!=', False),
@@ -85,18 +86,15 @@ class WizVendorStockReport(models.TransientModel):
                                                ('picking_type_code', '=', 'incoming'),
                                                ('state', 'not in', ('cancel', 'done'))],
                                               order='partner_id')
-            # purchase_order = purchase_order_obj.search([('partner_id', 'in', vendor.ids),
-            #                                                     ('is_shipped', '!=', True),
-            #                                                     ('state', 'in', ('purchase', 'done'))
-            #                                                     ], order='id')
+
             for pl in purchase_order.mapped('move_lines'):
                 if pl.picking_id.purchase_id and not pl.origin_returned_move_id:
                     po_move_ids.append(pl.id)
+                    existing_product_list.append(pl.product_id.id)
                     if pl.picking_id not in data.get(vendor).keys():
                         data.get(vendor).update({pl.picking_id: {pl.product_id: pl.product_uom_qty}})
                     else:
                         data.get(vendor).get(pl.picking_id).update({pl.product_id: pl.product_uom_qty})
-
             if not data.get(vendor):
                 product_tmpl_ids = self.env['product.supplierinfo'].search([('name', '=', vendor.id),
                                                                             ('product_tmpl_id.qty_available', '!=',
@@ -108,6 +106,7 @@ class WizVendorStockReport(models.TransientModel):
                 data.get(vendor).update({stock_obj: {}})
                 for prod in product_ids:
                     data.get(vendor).get(stock_obj).update({prod: 0.0})
+                    existing_product_list.append(prod.id)
 
         for vendor, value in data.items():
             is_val = len(list([v for v in value.values() if v]))
@@ -116,11 +115,15 @@ class WizVendorStockReport(models.TransientModel):
             # sheet.write(rows, cols, 'Vendor', title_format)
             # sheet.merge_range(rows, cols, rows, cols + 5,
             #                   vendor.name, title_format)
-            sheet.write(rows, cols, vendor.name, title_format)
+            # sheet.write(rows, cols, vendor.name, title_format)
+            sheet.write(rows, 0, vendor.name, title_format)
             rows += 1
             cols = 0
-            sheet.write(rows, cols, "Product", only_bold)
-            # cols += 1
+            sheet.write(rows, cols, "SKU", only_bold)
+            sheet.write(rows, 1, "Barcode", only_bold)
+            sheet.write(rows, 2, "Unit Barcode",only_bold)
+            sheet.write(rows, 3, "Product", only_bold)
+            cols = 3
             sheet.write(rows, cols + 1, "Stock in hand", only_bold)
             po_rows = rows
             po_cols = cols + 1
@@ -182,8 +185,14 @@ class WizVendorStockReport(models.TransientModel):
                         pd.get(product)['cols'] += 1
                         pd.get(product)['subtotal'] += qty
                         pd.get(product)['forecast_qty'] += qty
-                    sheet.write(pd.get(product).get('rows'), cols, product.name, value_format)
-                    sheet.write(pd.get(product).get('rows'), cols + 1, product.qty_available, right_format)
+
+                    cols = 0
+
+                    sheet.write(pd.get(product).get('rows'), cols, product.default_code, value_format)
+                    sheet.write(pd.get(product).get('rows'), cols + 1, product.barcode or '', value_format)
+                    sheet.write(pd.get(product).get('rows'), cols + 2, product.unit_barcode or '', value_format)
+                    sheet.write(pd.get(product).get('rows'), cols + 3, product.name, value_format)
+                    sheet.write(pd.get(product).get('rows'), cols + 4, product.qty_available, right_format)
                     if po:
                         sheet.write(pd.get(product).get('rows'), pd.get(product).get('cols'), qty, right_format)
                     sheet.write(pd.get(product).get('rows'), po_len + 1, pd.get(product).get('subtotal'),
@@ -197,6 +206,57 @@ class WizVendorStockReport(models.TransientModel):
                                     right_format)
             po_rows += 1
             rows += 2
+        sheet.write(rows, 0, 'No Vendor', title_format)
+        rows += 1
+        sheet.write(rows, cols, "SKU", only_bold)
+        sheet.write(rows, cols + 1, "Barcode", only_bold)
+        sheet.write(rows, cols + 2, "Unit Barcode", only_bold)
+        sheet.write(rows, cols + 3, "Product", only_bold)
+        sheet.write(rows, cols + 4, "Stock in hand", only_bold)
+        sheet.write(rows, cols + 5, "Total Sales / Outgoing", only_bold)
+        sheet.write(rows, cols + 6, "Incoming Return Qty", only_bold)
+        sheet.write(rows, cols + 7, "Total Forecast", only_bold)
+        sheet.write(rows, cols + 8, "Odoo Forecast", only_bold)
+        cols = 3
+        data.update({'no_vendor': {}})
+        no_vendor_product = self.env['product.product'].search(
+            [('id', 'not in', existing_product_list)])
+        for pd in no_vendor_product:
+            # if pd.qty_available:
+            qty_available = pd.qty_available
+            outgoing_move = move_obj.search(
+                [('product_id', '=', pd.id),
+                 ('location_id.usage', 'in', ('internal', 'transit')),
+                 ('location_dest_id.usage', 'not in',
+                  ('internal', 'transit')),
+                 ('state', 'not in', ('cancel', 'done'))],
+                order='partner_id')
+            so_qty = sum(outgoing_move.mapped('product_uom_qty'))
+            incoming_move = move_obj.search(
+                [('product_id', '=', pd.id),
+                 # ('id', '=', 14569),
+                 ('id', 'not in', tuple(po_move_ids)),
+                 ('location_id.usage', 'not in', ('internal', 'transit')),
+                 ('location_dest_id.usage', 'in', ('internal', 'transit')),
+                 ('state', 'not in', ('cancel', 'done'))],
+                order='partner_id')
+            rtn_incoming_qty = sum(incoming_move.mapped('product_uom_qty'))
+            qty = pd.qty_available + rtn_incoming_qty - so_qty
+            rows += 1
+            cols = 0
+            sheet.write(rows, cols, pd.product_tmpl_id.default_code or '', value_format)
+            sheet.write(rows, cols + 1, pd.product_tmpl_id.barcode or '', value_format)
+            sheet.write(rows, cols + 2, pd.product_tmpl_id.unit_barcode or '', value_format)
+            sheet.write(rows, cols + 3, pd.product_tmpl_id.name, value_format)
+            sheet.write(rows, cols + 4, pd.product_tmpl_id.qty_available, right_format)
+            sheet.write(rows, cols + 5, so_qty, right_format)
+            sheet.write(rows, cols + 6, rtn_incoming_qty, right_format)
+            sheet.write(rows, cols + 7, qty, right_format)
+            if self.odoo_Forecast:
+                sheet.write(rows, cols + 8, pd.product_tmpl_id.virtual_available, right_format)
+
+        sheet.set_column(0, 3, 25)
+        sheet.set_column(4, 30, 14)
 
         workbook.close()
         vendorstock_file = base64.b64encode(open('/tmp/' +
