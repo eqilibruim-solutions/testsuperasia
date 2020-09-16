4623d6ccb59e0a94c26bd9ae8e87cadcdaeed616
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-09-16 17:50:50 +0530

    [ADD] Added new module for superasia invoice reports.

---
+++ super_asia_invoice_report/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Bista Solutions Pvt. Ltd
+#    Copyright (C) 2019 (http://www.bistasolutions.com)
+#
+##############################################################################
+{
+    'name': 'Superasia Invoice Reports',
+    'version': '1.0',
+    'category': 'Reports',
+    'summary': 'Superasia Invoice Report',
+    'author': "Bista Solutions",
+    'website': 'http://www.bistasolutions.com',
+    'depends': ['base','account'],
+    'data': [
+        'views/invoice_sales_rep_report_wizard.xml',
+        'views/superasia_report.xml',
+    ],
+    'installable': True,
+}
---
+++ super_asia_invoice_report/wizard/__init__.py
@@ -0,0 +1,10 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    Bista Solutions Pvt. Ltd
+#    Copyright (C) 2019 (http://www.bistasolutions.com)
+#
+##############################################################################
+
+# from . import superasia_sales_rep_report_excel
+from . import invoice_sales_rep_report_wizard
