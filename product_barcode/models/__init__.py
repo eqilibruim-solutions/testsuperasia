8880d7c730adb1b3571abf64ad240483fcacae82
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-09-28 12:59:46 +0530

    [REM] Resolved conflict.

--- product_barcode/models/__init__.py
+++ product_barcode/models/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import product
-<<<<<<< HEAD
-=======
 from . import stock_picking
 from . import stock_location
->>>>>>> 2b7fcdc... [FIX] Fixed issue in merp module and added sequence field on location to sort desc wise.
