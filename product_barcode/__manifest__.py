de19aa03af989ce9f7029632123c64dd07a34c11
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-09-28 14:19:22 +0530

    [IMP] fixed issue in product_barcode module.

--- product_barcode/__manifest__.py
+++ product_barcode/__manifest__.py
@@ -12,13 +12,15 @@
     'website': "http://www.bistasolutions.com",
     'category': 'Product',
     'version': '1.2',
-    'depends': ['base', 'product', 'stock'],
+    'depends': ['base', 'product', 'stock', 'stock_barcode'],
     # always loaded
     'data': [
         'views/product_view.xml',
         'views/stock_barcode_templates.xml',
         'views/stock_location.xml',
     ],
+    'qweb': [
+        "static/src/xml/qweb_templates.xml",],
     # only loaded in demonstration mode
     'demo': [],
 }
