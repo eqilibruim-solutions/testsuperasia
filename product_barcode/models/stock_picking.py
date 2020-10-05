304787df92c8c3fb131c34a879a545c861036430
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-10-06 01:05:10 +0530

    [FIX] Fixed issue lineActions undefined.

--- product_barcode/models/stock_picking.py
+++ product_barcode/models/stock_picking.py
@@ -93,12 +93,12 @@ class StockPicking(models.Model):
         else:
             return (True, 'incoming_transfer')
 
-    def check_lot_on_barcode_scanned(self, barcode=None):
+    def check_lot_on_barcode_scanned(self, barcode=None, qty=None):
         '''This methodu used to check the lot/serial number is part of order or not.'''
         ml_id = self.move_line_ids_without_package.filtered(
             lambda
                 rec: rec.move_id and rec.lot_id.name == barcode)
-        if self.picking_type_code in ('outgoing', 'internal'):
+        if self.picking_type_code in ('outgoing', 'internal', 'incoming'):
             if ml_id:
                 return (True, 'existing_lot')
             else:
