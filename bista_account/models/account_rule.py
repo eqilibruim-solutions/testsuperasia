65598931c123b4f7b94ca0750e3d1c7bd91d5807
Ronak Chauhan <ronak.chauhan@bistasolutions.com>
2020-11-03 20:46:33 +0530

    [ADD] Added access rights for dues,payments and customer.

--- bista_account/models/account_rule.py
+++ bista_account/models/account_rule.py
@@ -8,14 +8,50 @@ class AccountMove(models.Model):
 
     @api.model
     def search(self, args, offset=0, limit=None, order=None, count=False):
-        print('============================================')
         if self.env.user.has_group('bista_account.group_own_customer'):
-            print('============dddddddddddd================================')
-            args += [('partner_id.user_id','=', self.env.user.id)]
+            args += [('user_id','=', self.env.user.id)]
         return super(AccountMove, self).search(args,offset,limit,order,count)
 
     @api.model
     def read_group(self, domain, fields, groupby, offset=0, limit=None, orderby=False, lazy=True):
         if self.env.user.has_group('bista_account.group_own_customer'):
-            domain += [('partner_id.user_id', '=', self.env.user.id)]
+            domain += [('user_id', '=', self.env.user.id)]
         return super(AccountMove, self).read_group(domain, fields, groupby, offset, limit, orderby, lazy)
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        if self.env.user.has_group('bista_account.group_own_customer'):
+            args += [('user_id', '=', self.env.user.id)]
+        return super(ResPartner, self).search(args, offset, limit, order,
+                                               count)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None,
+                   orderby=False, lazy=True):
+        if self.env.user.has_group('bista_account.group_own_customer'):
+            domain += [('user_id', '=', self.env.user.id)]
+        return super(ResPartner, self).read_group(domain, fields, groupby,
+                                                   offset, limit, orderby,
+                                                   lazy)
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        if self.env.user.has_group('bista_account.group_own_customer'):
+            args += [('partner_id.user_id', '=', self.env.user.id)]
+        return super(AccountPayment, self).search(args, offset, limit, order,
+                                               count)
+
+    @api.model
+    def read_group(self, domain, fields, groupby, offset=0, limit=None,
+                   orderby=False, lazy=True):
+        if self.env.user.has_group('bista_account.group_own_customer'):
+            domain += [('partner_id.user_id', '=', self.env.user.id)]
+        return super(AccountPayment, self).read_group(domain, fields, groupby,
+                                                   offset, limit, orderby,
+                                                   lazy)
