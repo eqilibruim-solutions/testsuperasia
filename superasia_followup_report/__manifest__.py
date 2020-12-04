dddd4d2814185d18d9348d066b4e2f9113d3f8d7
Ben <bmu@odoo.com>
2020-12-04 14:51:57 -0800

    update manifest

--- superasia_followup_report/__manifest__.py
+++ superasia_followup_report/__manifest__.py
@@ -21,7 +21,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'account_reports', 'base'],
+    'depends': ['account', 'account_followup', 'base'],
 
     # always loaded
     'data': [
