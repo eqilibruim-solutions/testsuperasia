1ae699bbec50c2eb6113d09b2dbc5609a6486591
Aqeeb Imtiaz <aqeeb.imtiaz@bistasolutions.com>
2020-12-30 21:06:04 +0600

    [UPDATE] auth_signup & PWA module

--- bista_web_pwa/__manifest__.py
+++ bista_web_pwa/__manifest__.py
@@ -2,14 +2,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 
 {
-    "name": "Progressive web application",
-    "summary": "Make Odoo a PWA",
+    "name": "Progressive web application by Bista",
+    "summary": "PWA module for Odoo",
     "version": "13.0.1.0.1",
     "development_status": "Beta",
     "category": "Website",
-    "website": "https://github.com/OCA/web",
-    "author": "TAKOBI, Odoo Community Association (OCA)",
-    "maintainers": ["eLBati"],
+    "website": "https://www.bistasolutions.com",
+    "author": "Bista Solutions",
     "license": "LGPL-3",
     "application": True,
     "installable": True,
