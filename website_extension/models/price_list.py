e7411ce5c0fd12b6931a9bbb395d4f3bee7c863e
Aqeeb Imtiaz <aqeeb.imtiaz@bistasolutions.com>
2021-01-29 22:37:44 +0600

    [ADD] user group wise price list

--- website_extension/models/price_list.py
+++ website_extension/models/price_list.py
@@ -1,6 +1,12 @@
 from odoo import fields, models, api
 
 
+class res_groups(models.Model):
+    _inherit = 'res.groups'
+
+    e_commerce = fields.Boolean(string="E-commerce")
+
+
 class PricelistExtension(models.Model):
     _inherit = 'product.pricelist'
 
@@ -8,3 +14,4 @@ class PricelistExtension(models.Model):
         ('person', 'Individual'),
         ('company', 'Company')
     ], string="Customer Type")
+    group_id = fields.Many2one('res.groups', string="User Type", domain="[('e_commerce', '=', True)]") # ToDo: domain set 'e_commerce' as true
--- website_extension/models/website.py
+++ website_extension/models/website.py
@@ -1,13 +1,41 @@
 from odoo import models, api, _
-# from odoo.http import request
+from odoo.http import request
 
 
 class Website(models.Model):
     _inherit = 'website'
 
-    def _get_pricelist_available(self, req, show_visible=False):
-        dep = super(Website, self)._get_pricelist_available(req, show_visible)
+    def get_pricelist_available(self, show_visible=False):
+        print("get_pricelist")
+        dep = super(Website, self).get_pricelist_available(show_visible)
 
-        print(dep)
+        req = request
+        # print(request)
 
-        return dep
+        # print(type(dep))
+        # sel_groups_1_8_9
+        # for pl in dep.read(['group_id']):
+        #     print(pl)
+        # if self.env.user.customer_type is not False:
+        #     if self.env.user.customer_type == 'person':
+        #         print('B2C Customer')
+        #     elif self.env.user.customer_type == 'company':
+        #         print('B2B Customer')
+        # else:
+        #     print(self.env.user.customer_type)
+
+        # if self.env.user.user_has_groups('base.group_portal'):
+        #     group_id = request.env['ir.model.data'].get_object('base', 'group_portal')
+        # else:
+        #     group_id = request.env['ir.model.data'].get_object('base', 'group_public')
+        if self.env.user.user_has_groups('superasiab2b_b2c.group_b2baccount'):
+            group_id = request.env['ir.model.data'].get_object('superasiab2b_b2c', 'group_b2baccount')
+        else:
+            group_id = request.env['ir.model.data'].get_object('superasiab2b_b2c', 'group_b2c')
+
+        group_list = [group_id.id]
+        print(group_list)
+
+        print(dep.search([('group_id', 'in', group_list)]))
+
+        return dep.search([('group_id', 'in', group_list)])
