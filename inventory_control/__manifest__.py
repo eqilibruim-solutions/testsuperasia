52ecd8cbabfe585c0db2cb645f648147d884cd23
Usaid Khan <usaid.khan@bistasolutions.com>
2021-05-20 11:07:22 -0400

    [ADD] stock bin to barcode; darshan merge

--- inventory_control/__manifest__.py
+++ inventory_control/__manifest__.py
@@ -8,10 +8,12 @@
     'website': "http://www.bistasolutions.com",
     'category': 'Product',
     'version': '1.1',
-    'depends': ['base','product','stock'],
+    'depends': ['base','product','stock','stock_barcode'],
     # always loaded
     'data': [
         'views/views.xml',
-        'views/product_product_view.xml',
+    ],
+    'qweb': [
+        "static/src/xml/barcode_extended.xml",
     ],
 }
--- inventory_control/models/models.py
+++ inventory_control/models/models.py
@@ -3,16 +3,16 @@ from odoo import models, fields
 class RealizedProductTemplate(models.Model):
     _inherit = 'product.template'
 
-    location_bin = fields.Char('Bin Location', help='use this to identify bin location')
+    location_bin = fields.Char('Stock Bin', help='use this to identify Stock Bin')
     # product_id = fields.Many2one('product.product')
 
 class RealizedProductProduct(models.Model):
     _inherit = 'product.product'
 
-    location_bin = fields.Char(related="product_tmpl_id.location_bin", string='Bin Location', help='use this to identify bin location')
+    location_bin = fields.Char(related="product_tmpl_id.location_bin", string='Stock Bin', help='use this to identify Stock Bin')
 
 class StockMoveLine(models.Model):
     _inherit = 'stock.move.line'
 
-    location_bin = fields.Char(related="product_id.location_bin", string='Bin Location', help='use this to identify bin location')
+    location_bin = fields.Char(related="product_id.location_bin", string='Stock Bin', help='use this to identify Stock Bin')
 
