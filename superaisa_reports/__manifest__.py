9c49d527584287c420440f116b1c39a4c77bab63
Aqeeb Imtiaz <aqeeb.imtiaz@bistasolutions.com>
2021-07-06 14:55:41 +0600

    [ADD] po field in delivery slip & invoice report

--- superaisa_reports/__manifest__.py
+++ superaisa_reports/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '1.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account', 'stock', 'purchase', 'product_barcode'],
+    'depends': ['base', 'account', 'stock', 'purchase', 'product_barcode', 'sale'],
 
     # always loaded
     'data': [
@@ -33,6 +33,7 @@
         'reports/PO_inherit.xml',
         'reports/ckca_check_template_extended.xml',
         'reports/quotation_report_inherit.xml',
+        'reports/delivery_report_inherit.xml',
         # 'reports/frozen_picking_report_inherit.xml',
         # 'reports/report.xml',
     ],
