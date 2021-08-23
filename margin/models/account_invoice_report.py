e1192ad249a868185971f3b482521b7d85815506
saidul-islam-tuhin <tuhinmd718@gmail.com>
2021-08-23 22:22:07 +0600

    [FIXING] fixing field miss and product quantity of account invoice report

--- margin/models/account_invoice_report.py
+++ margin/models/account_invoice_report.py
@@ -11,42 +11,47 @@ class AccountInvoiceReport(models.Model):
     margin_subtotal_signed = fields.Float('Margin')
 
 
+    # @api.model
+    # def _select(self):
+    #     return '''
+    #         SELECT
+    #             line.id,
+    #             line.move_id,
+    #             line.product_id,
+    #             line.account_id,
+    #             line.analytic_account_id,
+    #             line.journal_id,
+    #             line.company_id,
+    #             SUM(line.margin_subtotal_signed) AS margin_subtotal_signed,
+    #             COALESCE(line.currency_id, line.company_currency_id)        AS currency_id,
+    #             line.partner_id AS commercial_partner_id,
+    #             move.name,
+    #             move.state,
+    #             move.type,
+    #             move.partner_id,
+    #             move.invoice_user_id,
+    #             move.fiscal_position_id,
+    #             move.invoice_payment_state,
+    #             move.invoice_date,
+    #             move.invoice_date_due,
+    #             move.invoice_payment_term_id,
+    #             move.invoice_partner_bank_id,
+    #             move.amount_residual_signed                                 AS residual,
+    #             move.amount_total_signed                                    AS amount_total,
+    #             uom_template.id                                             AS product_uom_id,
+    #             template.categ_id                                           AS product_categ_id,
+    #             line.quantity / NULLIF(COALESCE(uom_line.factor, 1) / COALESCE(uom_template.factor, 1), 0.0) * (CASE WHEN move.type IN ('in_invoice','out_refund','in_receipt') THEN -1 ELSE 1 END)
+    #                                                                         AS quantity,
+    #             -SUM(line.balance)                                          AS price_subtotal,
+    #             -SUM(line.balance / NULLIF(COALESCE(uom_line.factor, 1) * COALESCE(uom_template.factor, 1), 0.0))
+    #                                                                         AS price_average,
+    #             COALESCE(partner.country_id, commercial_partner.country_id) AS country_id,
+    #             1                                                           AS nbr_lines,
+    #             line.brand, move.ethnicity, move.channel, move.city, move.state_id
+    #     '''
     @api.model
     def _select(self):
-        return '''
-            SELECT
-                line.id,
-                line.move_id,
-                line.product_id,
-                line.account_id,
-                line.analytic_account_id,
-                line.journal_id,
-                line.company_id,
-                SUM(line.margin_subtotal_signed) AS margin_subtotal_signed,
-                COALESCE(line.currency_id, line.company_currency_id)        AS currency_id,
-                line.partner_id AS commercial_partner_id,
-                move.name,
-                move.state,
-                move.type,
-                move.partner_id,
-                move.invoice_user_id,
-                move.fiscal_position_id,
-                move.invoice_payment_state,
-                move.invoice_date,
-                move.invoice_date_due,
-                move.invoice_payment_term_id,
-                move.invoice_partner_bank_id,
-                move.amount_residual_signed                                 AS residual,
-                move.amount_total_signed                                    AS amount_total,
-                uom_template.id                                             AS product_uom_id,
-                template.categ_id                                           AS product_categ_id,
-                SUM(line.quantity / NULLIF(COALESCE(uom_line.factor, 1) * COALESCE(uom_template.factor, 1), 0.0))
-                                                                            AS quantity,
-                -SUM(line.balance)                                          AS price_subtotal,
-                -SUM(line.balance / NULLIF(COALESCE(uom_line.factor, 1) * COALESCE(uom_template.factor, 1), 0.0))
-                                                                            AS price_average,
-                COALESCE(partner.country_id, commercial_partner.country_id) AS country_id,
-                1                                                           AS nbr_lines
-        '''
+        return super(AccountInvoiceReport,
+                    self)._select() + ",SUM(line.margin_subtotal_signed) AS margin_subtotal_signed"
 
     
