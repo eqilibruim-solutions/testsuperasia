06fad6b6938d225d731a08b6ac8c0b259098da92
Archana Prasad <archana.prasad@bistasolutions.com>
2021-09-09 23:51:54 +0530

    MRP access to portal

--- superasiab2b_b2c/__manifest__.py
+++ superasiab2b_b2c/__manifest__.py
@@ -7,7 +7,7 @@
     'summary': '',
     'author':'Bista Solutions - Archana Prasad',
     'depends': [
-        'base','portal','sale','product','website','website_sale','sale_coupon','website_sale_stock'
+        'base','portal','sale','product','website','website_sale','sale_coupon','website_sale_stock','mrp'
     ],
     'data': [
             'security/access_rights.xml',
