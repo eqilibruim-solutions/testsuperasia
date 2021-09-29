5b270c4fb7c080bc57211ce02b5ca85cfa0c4d76
saidul-islam-tuhin <tuhinmd718@gmail.com>
2021-09-29 17:25:11 +0600

    [B2B Flow] Last order list and in cart btn in portal page

--- superasiab2b_b2c/__manifest__.py
+++ superasiab2b_b2c/__manifest__.py
@@ -13,10 +13,12 @@
             'security/access_rights.xml',
             'security/ir.model.access.csv',
             'data/data.xml',
+            'views/assets.xml',
             'views/portal_template.xml',
             'views/sales_order.xml',
             'views/inherited_product_template_meta_fields.xml',
             'views/website_config_settings.xml',
+            'views/sale_portal_templates.xml',
             'report/shipping_labels.xml'
             ],
 
