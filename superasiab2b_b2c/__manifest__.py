2c1675e9142ca60c6814d2d8e8e3908f229a8d6b
saidul-islam-tuhin <tuhinmd718@gmail.com>
2021-09-30 21:13:38 +0600

    [B2B Flow] create new mail template for b2b order

--- superasiab2b_b2c/__manifest__.py
+++ superasiab2b_b2c/__manifest__.py
@@ -19,7 +19,8 @@
             'views/inherited_product_template_meta_fields.xml',
             'views/website_config_settings.xml',
             'views/sale_portal_templates.xml',
-            'report/shipping_labels.xml'
+            'report/shipping_labels.xml',
+            'data/mail_data.xml',
             ],
 
         'qweb': [
