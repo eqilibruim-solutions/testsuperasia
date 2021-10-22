ce993e83447a4318a6df7eb3c44d3111bee82151
saidul-islam-tuhin <tuhinmd718@gmail.com>
2021-10-22 18:54:48 +0600

    [IMP]b2b_flow: style added in login wholesale price link

--- bista_superasia_theme/__manifest__.py
+++ bista_superasia_theme/__manifest__.py
@@ -8,7 +8,7 @@
     'author': "Bista Solutions Pvt. Ltd.",
     'website': 'www.bistasolutions.com',
     'license': 'AGPL-3',
-    "depends": ['base','web','website','portal','bista_web_pwa','superasiab2b_b2c','sale','payment','website_sale','website_form','auth_signup','website_sale_delivery','website_sale_stock','sale_stock'],
+    "depends": ['base','web','website','portal','bista_web_pwa','superasiab2b_b2c','sale','payment','website_sale','website_form','auth_signup','website_sale_delivery','website_sale_stock','sale_stock','website_crm'],
     "data": [
         'security/ir.model.access.csv',
         'views/assets.xml',
