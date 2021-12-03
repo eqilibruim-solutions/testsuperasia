e238298369467019dd4bfc31fc7fc509148db5c3
saidul-islam-tuhin <tuhinmd718@gmail.com>
2021-12-03 16:18:51 +0600

    [imp]source_id: store value in sale order based on partner.

--- superasiab2b_b2c/models/sale_order.py
+++ superasiab2b_b2c/models/sale_order.py
@@ -1,8 +1,23 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
+
+    @api.model
+    def create(self, vals):
+        if vals.get('partner_id'):
+            res_partner = self.env['res.partner'].browse(int(vals.get('partner_id')))
+            vals['source_id'] = res_partner.user_ids.source_id.id
+
+        result = super(SaleOrder, self).create(vals)
+        return result
+
+    @api.onchange('partner_id')
+    def onchange_partner_id_source_id(self):
+        if not self.partner_id:
+            return
+        self.source_id = self.partner_id.user_ids.source_id.id
 
     def _latest_quotation_sent_order(self, partner, **kwargs):
         self.ensure_one()
