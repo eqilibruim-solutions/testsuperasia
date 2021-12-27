c0bb562a3b028ad9a2857ba9fd754e180621871f
saidul-islam-tuhin <tuhinmd718@gmail.com>
2021-12-27 15:39:29 +0600

    [IMP] sale_app: seperate account create and update path

--- superasia_salesrep_app/models/sales_order.py
+++ superasia_salesrep_app/models/sales_order.py
@@ -4,7 +4,7 @@ from odoo import models, fields
 class SaleOrderSalesRep(models.Model):
     _inherit = 'sale.order'
     sales_rep_id = fields.Many2one(
-        'res.users', string='Sales Representative', index=True, default=lambda self: self.env.user,
+        'res.users', string='Sales Representative',
         domain=lambda self: [('groups_id', 'in', self.env.ref('superasia_salesrep_app.group_sales_rep').id)])
     
     def _website_product_id_change(self, order_id, product_id, qty=0):
