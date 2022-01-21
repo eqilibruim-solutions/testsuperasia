3b66921cb817fe370ab0558df85b48d13c1d13ed
saidul-islam-tuhin <tuhinmd718@gmail.com>
2022-01-21 19:05:00 +0600

    [IMP] Hide price,add cart btn for guest user and consumer login

--- bista_superasia_theme/static/src/js/website_sale_recently_viewed.js
+++ bista_superasia_theme/static/src/js/website_sale_recently_viewed.js
@@ -2,8 +2,22 @@ odoo.define('bista_superasia_theme.recently_viewed_ept', function (require) {
     "use strict";
     var publicWidget = require('web.public.widget');
     var wSaleUtils = require('website_sale.utils');
+    var session = require('web.session');
+    var config = require('web.config');
 
     publicWidget.registry.productsRecentlyViewedSnippet.include({
+        
+        /**
+         * @override
+         * Hide carousal footer(product price and Add to cart) for guest user
+         */
+        _addCarousel: function () {
+            var carousel = config.device.size_class <= config.device.SIZES.SM ? this.mobileCarousel : this.webCarousel;
+            if(!session.user_id) {
+                carousel.find(".o_carousel_product_card_footer").remove();
+            }
+            this.$('.slider').html(carousel).css('display', ''); // TODO removing the style is useless in master
+        },
         /**
          @override
          Reload the page after adding product.
@@ -28,6 +42,6 @@ odoo.define('bista_superasia_theme.recently_viewed_ept', function (require) {
                 //     self._render(values[0]);
                 // });
             });
-        }
+        },
     });
 });
